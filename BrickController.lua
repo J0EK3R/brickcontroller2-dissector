cfa3c32acdfd8a4b138bf57e663650b7cad27c51
J0EK3R <J0EK3R@gmx.net>
2025-06-30 10:42:15 +0200

    JK: bugfix

--- BrickController.lua
+++ BrickController.lua
@@ -7,7 +7,7 @@ local CryptTools = {}
 -- Helper: block-copy len bytes from src at srcOffset to dest at destOffset.
 local function block_copy(src, srcOffset, dest, destOffset, length)
     for i = 0, length - 1 do
-        dest[destOffset] = src[srcOffset]
+        dest[destOffset + i] = src[srcOffset + i]
     end
 end
 
@@ -262,45 +262,6 @@ function CryptTools.DecryptRfPayload(seed, headerLength, dataLength, headerOffse
 
     return dataOut
 end
-
-function table_print (tt, indent, done)
-  done = done or {}
-  indent = indent or 0
-  if type(tt) == "table" then
-    local sb = {}
-    for key, value in pairs (tt) do
-      table.insert(sb, string.rep (" ", indent)) -- indent it
-      if type (value) == "table" and not done [value] then
-        done [value] = true
-        table.insert(sb, key .. " = {\n");
-        table.insert(sb, table_print (value, indent + 2, done))
-        table.insert(sb, string.rep (" ", indent)) -- indent it
-        table.insert(sb, "}\n");
-      elseif "number" == type(key) then
-        table.insert(sb, string.format("\"%s\"\n", tostring(value)))
-      else
-        table.insert(sb, string.format(
-            "%s = \"%s\"\n", tostring (key), tostring(value)))
-       end
-    end
-    return table.concat(sb)
-  else
-    return tt .. "\n"
-  end
-end
-
-function to_string( tbl )
-    if  "nil"       == type( tbl ) then
-        return tostring(nil)
-    elseif  "table" == type( tbl ) then
-        return table_print(tbl)
-    elseif  "string" == type( tbl ) then
-        return tbl
-    else
-        return tostring(tbl)
-    end
-end
-
 
 -- Define a new protocol for post-dissector usage.
 local myproto = Proto("myproto", "My Global Frame Analyzer")
